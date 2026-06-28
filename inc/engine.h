5570c6b1eedf84568c1fd424775ea07530179c51
Bichael <mpwilkinson007@gmail.com>
2026-06-28 15:25:20 +0100

    ghost, ghouls and goblins - changed the delete method - finished the easiest bits

--- inc/engine.h
+++ inc/engine.h
@@ -1,28 +1,31 @@
 #pragma once
 #include <memory>
+#include <vk_mem_alloc.h>
 #include <vulkan/vulkan_raii.hpp>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <ecs.h>
 
 namespace Arawn {
 	enum class DisplayMode { WINDOWED, FULLSCREEN, EXCLUSIVE };
 	enum class BufferingMode { DOUBLE, TRIPLE };
-	enum class VsyncMode { NONE, VYSNC };
-	struct EngineSettings { 
-		const char* appName;
+	enum class VsyncMode { DISABLED, ENABLED };
+	enum class LowLatencyMode { DISABLED, BALANCED, ENABLED };
+	struct AppSettings { 
+		const char* appName = "";
 		uint32_t appVersion;
-		const char* engineName;
-		uint32_t engineVersion;
-		const char* gpu;
+		// API version??
+		// extensions??
 	};
-
+	
 	struct DisplaySettings {
-		struct { uint32_t x, y; } resolution;
-		
-		DisplayMode display;
-		BufferingMode buffering;
-		uint32_t refreshRate;
-
+		const char* gpu = nullptr;
+		DisplayMode mode = DisplayMode::WINDOWED;
+		struct { uint32_t x, y; } resolution = { 800, 600 };
+		uint32_t refreshRate = 60;
+		VsyncMode vsync = VsyncMode::ENABLED;
+		LowLatencyMode latency = LowLatencyMode::BALANCED;
+		BufferingMode buffering = BufferingMode::DOUBLE;
 	};
 
 	enum class MeshQuality { LOW, MEDIUM, HIGH };
@@ -30,61 +33,73 @@ namespace Arawn {
 	enum class ShadowQuality { NONE, LOW, MEDIUM, HIGH };
 	enum class AmbientOcclusionQuality { NONE, LOW, MEDIUM, HIGH };
 	enum class GlobalIlluminationQuality { NONE, LOW, MEDIUM, HIGH };
+	enum class ScreenSpaceReflectionQuality { NONE, LOW, MEDIUM, HIGH };
 	enum class AntiAliasing { NONE, FXAA, MSAA_2X, MSAA_4X, TAA };
 
 	struct GraphicsSettings {
 		MeshQuality mesh;
 		TextureQuality texture;
 		ShadowQuality shadow;
-		AmbientOcclusionQuality ao;
-		GlobalIlluminationQuality gi;
+		AmbientOcclusionQuality ambientOcclusion;
+		GlobalIlluminationQuality globalIllumination;
+		ScreenSpaceReflectionQuality ssr;
+	};
+
+	struct Settings {
+		AppSettings app = {};
+		DisplaySettings display = {};
+		GraphicsSettings graphics = {};
 	};
 
 	class Engine {
 	public:
+		Engine(const Settings& settings = {});
+		Engine& operator=(const Settings& settings);
+		Engine& operator=(const DisplaySettings& settings);
+		Engine& operator=(const GraphicsSettings& settings);
+
+	private:
 		struct Queue { 
 			uint32_t family, index;
 			vk::Queue queue;
 		};
-		using Window = std::unique_ptr<GLFWwindow, void(*)(GLFWwindow*)>;
-	
-	public:
-		Engine(const EngineSettings& engine = {}, const DisplaySettings& display = {}, const GraphicsSettings& graphics = {});
-
-		void recreate();
-
-		void apply(const GraphicsSettings& graphics);
-
-		void apply(const DisplaySettings& display) { apply(display, settings.graphics); }
-		void apply(const DisplaySettings& display, const GraphicsSettings& graphics);
+		struct VmaAllocatorDeleter { void operator()(VmaAllocator_T*); };
+		using VmaAllocatorRAII = std::unique_ptr<VmaAllocator_T, VmaAllocatorDeleter>;
 		
-	private:
+		struct GLFWwindowDeleter { void operator()(GLFWwindow*); };
+		using WindowRAII = std::unique_ptr<GLFWwindow, GLFWwindowDeleter>;
+
+		
 		vk::raii::Instance createInstance();
+		#if ARAWN_DEBUG
+		vk::raii::DebugUtilsMessengerEXT createDebugMessenger();
+		#endif
+		WindowRAII createWindow();
+		vk::raii::SurfaceKHR createSurface();
 		vk::raii::PhysicalDevice selectGPU();
 		vk::raii::Device createDevice();
-		Window createWindow();
-		vk::raii::SurfaceKHR createSurface();
+		VmaAllocatorRAII createAllocator();
 		vk::raii::SwapchainKHR createSwapchain();
-
-		struct {
-			EngineSettings engine;
-			DisplaySettings display;
-			GraphicsSettings graphics;
-		} settings;
-
+		
+		Settings settings;
+		
 		vk::raii::Context context;
 		vk::raii::Instance instance;
-		vk::raii::PhysicalDevice gpu;
-		vk::raii::Device device;
-		Queue graphics, compute, transfer, async;
-
-		Window window;
+		#if ARAWN_DEBUG
+		vk::raii::DebugUtilsMessengerEXT debugger;
+		#endif
+		
+		WindowRAII window;
 		vk::raii::SurfaceKHR surface;
+		
+		vk::raii::PhysicalDevice gpu;
+		Queue graphics, compute, transfer, present;
+		vk::raii::Device device;
+		VmaAllocatorRAII allocator;
+		
+		vk::SurfaceFormatKHR surfaceFormat;
 		vk::raii::SwapchainKHR swapchain;
 
-
-
+		// ecs::registry<Mesh, Image, Buffer, Program> registry;
 	};
 }
-
-
--- src/engine.cpp
+++ src/engine.cpp
@@ -1,140 +1,466 @@
+#define VMA_IMPLEMENTATION
 #include <engine.h>
+#include <ranges>
 
-Arawn::Engine::Engine(const EngineSettings& engine, const DisplaySettings& display, const GraphicsSettings& graphics) 
- : settings(engine, display, graphics)
+const std::vector<const char*> instanceExtensions = {
+#if ARAWN_DEBUG
+	VK_EXT_DEBUG_UTILS_EXTENSION_NAME
+#endif
+};
+const std::vector<const char*> deviceExtensions = {
+	VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+	VK_KHR_DYNAMIC_RENDERING_EXTENSION_NAME
+};
+
+const std::vector<const char*> debugLayers {
+#if ARAWN_DEBUG
+	"VK_LAYER_KHRONOS_validation"
+#endif
+};
+
+Arawn::Engine::Engine(const Settings& settings) 
+ : settings(settings)
  , instance(createInstance())
- , gpu(selectGPU())
- , device(createDevice())
+ #if ARAWN_DEBUG
+ , debugger(createDebugMessenger())
+ #endif
  , window(createWindow())
  , surface(createSurface())
+ , gpu(selectGPU())
+ , device(createDevice())
+ , allocator(createAllocator())
  , swapchain(createSwapchain()) {
-
-
-
-
+	
 }
 
 vk::raii::Instance Arawn::Engine::createInstance() {
-	vk::ApplicationInfo app_info(settings.engine.appName, settings.engine.appVersion, settings.engine.engineName, settings.engine.engineVersion, VK_API_VERSION_1_4);
+	if (!glfwInit()) throw std::runtime_error("error: failed to init glfw");
+
+	vk::ApplicationInfo app_info(settings.app.appName, settings.app.appVersion, "Arawn", 1, VK_API_VERSION_1_3);
 	vk::InstanceCreateInfo info({}, &app_info);
 	
-	std::vector<const char*> extensions = { };
-	info.setPEnabledExtensionNames(extensions);
+	std::vector<const char*> required = instanceExtensions;
+	uint32_t glfwExtCount;
+	const char** glfwExts = glfwGetRequiredInstanceExtensions(&glfwExtCount);
+	required.insert(required.end(), glfwExts, glfwExts + glfwExtCount);
+	
+	std::ranges::sort(required, [](const char* lhs, const char* rhs)->bool {
+		return std::strcmp(lhs, rhs);
+	});
+	
+	required.erase(std::ranges::unique(required, [](const char* lhs, const char* rhs)->bool {
+		return std::strcmp(lhs, rhs) == 0;
+	}).end(), required.end());
 
-	std::vector<const char*> layers = { };
-	info.setPEnabledLayerNames(layers);
+	info.setPEnabledExtensionNames(required);
+	info.setPEnabledLayerNames(debugLayers);
 	
 	return vk::raii::Instance(context, info);
 }
 
-vk::raii::PhysicalDevice Arawn::Engine::selectGPU() {
-	auto devices = instance.enumeratePhysicalDevices();
+#if ARAWN_DEBUG
+VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(vk::DebugUtilsMessageSeverityFlagBitsEXT messageSeverity, vk::DebugUtilsMessageTypeFlagsEXT messageType, const vk::DebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData) {
+	const char* severity;
+	switch (messageSeverity) {
+	case vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose: 
+		severity = "verbose";
+		break;
+	case vk::DebugUtilsMessageSeverityFlagBitsEXT::eInfo: 
+		severity = "info";
+		break;
+	case vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning: 
+		severity = "warning";
+		break;
+	case vk::DebugUtilsMessageSeverityFlagBitsEXT::eError: 
+		severity = "error";
+		break;
+	}
 
-	if (devices.empty()) {
-		throw std::runtime_error("no device found");
-	}
-	
-	for (auto& device : devices) {
-		if (strcmp(device.getProperties().deviceName, settings.engine.gpu)) {
-			return device;
-		}
-	}
-	
-	for (auto& device : devices) {
-		if (device.getProperties().deviceType == vk::PhysicalDeviceType::eDiscreteGpu) {
-			return device;
-		}
-	}
-	
-	return devices[0];
+	std::printf("[vulkan %s]: %s\n", severity, pCallbackData->pMessage);
+	return VK_FALSE;
 }
 
-vk::raii::Device Arawn::Engine::createDevice() {
+vk::raii::DebugUtilsMessengerEXT Arawn::Engine::createDebugMessenger() {
+    vk::DebugUtilsMessengerCreateInfoEXT info{
+		{}, 
+		vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning
+		| vk::DebugUtilsMessageSeverityFlagBitsEXT::eError,
+        vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral
+		| vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation
+		| vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance,
+		debugCallback,
+		nullptr
+	};
+	
+	return instance.createDebugUtilsMessengerEXT(info);
+}
+#endif
+
+Arawn::Engine::WindowRAII Arawn::Engine::createWindow() {
+	GLFWwindow* window;
+
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	glfwWindowHint(GLFW_REFRESH_RATE, settings.display.refreshRate);
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+	switch(settings.display.mode) {
+	case DisplayMode::WINDOWED: {
+		glfwWindowHint(GLFW_DECORATED, GLFW_TRUE);
+		glfwWindowHint(GLFW_FLOATING, GLFW_FALSE);
+
+		window = glfwCreateWindow(
+			settings.display.resolution.x,
+			settings.display.resolution.y,
+			settings.app.appName,
+			nullptr,
+			nullptr
+		);
+		break;
+	}
+	case DisplayMode::FULLSCREEN: {
+		GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+		const GLFWvidmode* vidmode = glfwGetVideoMode(monitor);
+		
+		glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
+		glfwWindowHint(GLFW_FLOATING, GLFW_TRUE);
+
+		window = glfwCreateWindow(
+			vidmode->width, 
+			vidmode->height,
+			settings.app.appName,
+			nullptr,
+			nullptr
+		);
+		break;
+	}
+	case DisplayMode::EXCLUSIVE: {
+		glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
+		glfwWindowHint(GLFW_FLOATING, GLFW_TRUE);
+	
+		window = glfwCreateWindow(
+			settings.display.resolution.x,
+			settings.display.resolution.y,
+			settings.app.appName,
+			glfwGetPrimaryMonitor(),
+			nullptr
+		);
+		break;
+	}	
+	}
+
+	if (!window) [[unlikely]] {
+		const char* message;
+		glfwGetError(&message);
+
+		std::printf("glfw error: %s\n", message);
+		throw std::runtime_error("");
+	}
+	return WindowRAII(window);
+}
+
+vk::raii::SurfaceKHR Arawn::Engine::createSurface() {
+	VkSurfaceKHR surface;
+	if (glfwCreateWindowSurface(*instance, window.get(), nullptr, &surface) != VK_SUCCESS) {
+		const char* message;
+		glfwGetError(&message);
+		std::printf("glfw error: %s\n", message);
+		throw std::runtime_error("failed to create surface");
+	}
+	return vk::raii::SurfaceKHR(instance, surface);
+}
+
+vk::raii::PhysicalDevice Arawn::Engine::selectGPU() {
+	auto unsupportedGPU = [&](vk::PhysicalDevice gpu) { 
+		// properties
+		auto properties = gpu.getProperties();
+		if (properties.deviceType == vk::PhysicalDeviceType::eCpu) {
+			return true;
+		}
+		
+		auto families = gpu.getQueueFamilyProperties();
+		// queue requirements
+		if (std::ranges::none_of(families, [](const auto& family)->bool { 
+			return (family.queueFlags & vk::QueueFlagBits::eGraphics) == vk::QueueFlagBits::eGraphics;
+		})) return true;
+
+		if (std::ranges::none_of(families, [](const auto& family)->bool { 
+			return (family.queueFlags & vk::QueueFlagBits::eCompute) == vk::QueueFlagBits::eCompute;
+		})) return true;
+
+		if (std::ranges::none_of(families, [](const auto& family)->bool { 
+			return (family.queueFlags & vk::QueueFlagBits::eTransfer) == vk::QueueFlagBits::eTransfer;
+		})) return true;
+
+		if (std::ranges::none_of(std::views::iota(static_cast<uint32_t>(0), static_cast<uint32_t>(families.size())), [&](uint32_t i)->bool { 
+			return gpu.getSurfaceSupportKHR(i, surface);
+		})) return true;
+
+		// extensions
+		auto extensions = gpu.enumerateDeviceExtensionProperties();
+		for (const char* ext : deviceExtensions) {
+			if (std::ranges::none_of(extensions, [ext](const auto& candidate) {
+				return strcmp(candidate.extensionName, ext);
+			})) return true;
+		}
+
+		// features
+		// auto features = gpu.getFeatures();
+
+		return false;
+	};
+
+	auto scoreGPU = [&](vk::PhysicalDevice gpu)->uint32_t {
+		uint32_t score = 0;
+		auto props = gpu.getProperties();
+		auto features = gpu.getFeatures();
+
+		if (props.deviceType == vk::PhysicalDeviceType::eDiscreteGpu) {
+			score += 10000;
+		}
+
+		auto memProps = gpu.getMemoryProperties();
+		for (const auto& heap : memProps.memoryHeaps) {
+			if (heap.flags & vk::MemoryHeapFlagBits::eDeviceLocal) {
+				score += (heap.size / (1024 * 1024 * 1024)) * 50;
+			}
+		}
+
+		return score;
+	};
+
+	auto gpus = instance.enumeratePhysicalDevices();
+	
+	if (settings.display.gpu == nullptr) {
+		std::printf("no device selected...\n");
+	} else if (auto it = std::ranges::find_if(gpus, [&](const auto& gpu) { 
+		return strcmp(gpu.getProperties().deviceName, settings.display.gpu);
+	}); it == gpus.end()) {
+		std::printf("device \"%s\" not found...\n", settings.display.gpu);
+	} else if (unsupportedGPU(*it)) {
+		std::printf("device \"%s\" not suitable...\n", settings.display.gpu);
+		gpus.erase(it);
+	} else {
+		return *it;
+	}
+	
+	std::erase_if(gpus, unsupportedGPU);
+	if (gpus.empty()) throw std::runtime_error("no supported gpus found");
+	
+	auto selected = *std::ranges::max_element(gpus, {}, scoreGPU);
+	
+	settings.display.gpu = selected.getProperties().deviceName;	
+	std::printf("device \"%s\" selected\n", settings.display.gpu);
+	
+	return selected;
+}
+
+vk::raii::Device Arawn::Engine::createDevice() {	
 	vk::DeviceCreateInfo info;
+	vk::PhysicalDeviceFeatures features{ };
 	auto families = gpu.getQueueFamilyProperties();
 	
 	std::vector<vk::DeviceQueueCreateInfo> queueInfos(families.size());
-	for (uint32_t i = 0; i < families.size(); ++i) {
-		queueInfos[i].queueFamilyIndex = i;
+	for (uint32_t i = 0; i < families.size(); ++i) queueInfos[i].queueFamilyIndex = i;
+	
+	{ // assign queue family and index
+		auto assignQueue = [&](vk::QueueFlags required, vk::QueueFlags excluded, Queue& output) {
+			for (uint32_t i = 0; i < families.size(); ++i) {
+				auto& family = families[i];
+				auto& queueCount = queueInfos[i].queueCount;
+				
+				if ((family.queueFlags & required) != required) continue;
+				if ((family.queueFlags & excluded) != vk::QueueFlags()) continue;
+				if (queueCount == family.queueCount) continue;
+				
+				output.family = i;
+				output.index = queueCount++;
+				return;
+			}
+
+			for (uint32_t i = 0; i < families.size(); ++i) {
+				auto& family = families[i];
+				
+				if (!(family.queueFlags & required)) continue;
+
+				output.family = i;
+				output.index = 0;
+				return;
+			}
+
+			throw std::runtime_error("error: no suitable queue found on selected device.");
+		};
+
+		auto assignPresentQueue = [&](Queue& output) {
+			auto families = gpu.getQueueFamilyProperties();
+
+			for (uint32_t i = 0; i < families.size(); ++i) {
+				auto& family = families[i];
+				
+				if (!gpu.getSurfaceSupportKHR(i, *surface)) continue;
+				if (!(family.queueFlags & vk::QueueFlagBits::eGraphics)) continue;
+
+				output.family = i;
+				output.index = 0;
+
+				if (family.queueCount == 0) ++family.queueCount;
+				
+				return;
+			}
+
+			throw std::runtime_error("error: no suitable queue found on selected device.");
+		};
+		
+		assignQueue(vk::QueueFlagBits::eGraphics, {}, graphics);
+		assignQueue(vk::QueueFlagBits::eCompute, {}, compute);
+		assignQueue(vk::QueueFlagBits::eTransfer, vk::QueueFlagBits::eGraphics, transfer);
+		// requestQueue(vk::QueueFlagBits::eCompute, vk::QueueFlagBits::eGraphics, async);
+		assignPresentQueue(present);
+
 	}
-
-	auto requestQueue = [&](vk::QueueFlags required, vk::QueueFlags excluded, Arawn::Engine::Queue& output) {
-		for (uint32_t i = 0; i < families.size(); ++i) {
-			auto& family = families[i];
-			auto& queueCount = queueInfos[i].queueCount;
-			
-			if ((family.queueFlags & required) != required) continue;
-			if ((family.queueFlags & excluded) != vk::QueueFlags()) continue;
-			if (queueCount == family.queueCount) continue;
-			
-			output.family = i;
-			output.index = queueCount++;
-			return;
+	// assign priorities
+	std::vector<float> priorities(std::ranges::fold_left(queueInfos, 0, [](uint32_t count, vk::DeviceQueueCreateInfo& family) {
+		return count + family.queueCount;
+	}));
+	{
+		float* priority = priorities.data();
+		for (auto& family : queueInfos) {
+			family.setPQueuePriorities(priority);
+			priority += family.queueCount;
 		}
-
-		for (uint32_t i = 0; i < families.size(); ++i) {
-			auto& family = families[i];
-			
-			if (!(family.queueFlags & required)) continue;
-
-			output.family = i;
-			output.index = 0;
-			return;
-		}
-
-		throw std::runtime_error("failed to assign queue");
-	};
-
-	requestQueue(vk::QueueFlagBits::eGraphics, {}, graphics);
-	requestQueue(vk::QueueFlagBits::eCompute, {}, compute);
-	requestQueue(vk::QueueFlagBits::eTransfer, vk::QueueFlagBits::eGraphics, transfer);
-	requestQueue(vk::QueueFlagBits::eCompute, vk::QueueFlagBits::eGraphics, async);
 	
-	queueInfos.erase(std::remove_if(queueInfos.begin(), queueInfos.end(), [](const auto& info) {
+		auto assignPriority = [&](const Queue& input, float priority) {
+			std::max(priority, ((queueInfos[graphics.family].pQueuePriorities - priorities.data()) + priorities.data())[graphics.index]);
+		};
+	
+		assignPriority(graphics, 0.8);
+		assignPriority(compute, 0.6);
+		assignPriority(transfer, 0.4);
+		assignPriority(present, 1.0);
+
+	}
+	// reduce queues -> loses family mapping
+	std::erase_if(queueInfos, [](const auto& info) {
 		return info.queueCount == 0;
-	}), queueInfos.end());
-	info.setQueueCreateInfos(queueInfos);
+	});
 	
-	vk::PhysicalDeviceFeatures features{ };
+	info.setQueueCreateInfos(queueInfos);
 	info.setPEnabledFeatures(&features);
-
-	std::vector<const char*> extensions = { };
-	info.setPEnabledExtensionNames(extensions);
-
+	info.setPEnabledExtensionNames(deviceExtensions);
 	auto device = gpu.createDevice(info);
-
+	
 	graphics.queue = device.getQueue(graphics.family, graphics.index);
 	compute.queue = device.getQueue(compute.family, compute.index);
 	transfer.queue = device.getQueue(transfer.family, transfer.index);
-	async.queue = device.getQueue(async.family, async.index);
+	present.queue = device.getQueue(present.family, present.index);
 	
 	return device;
 }
 
-Arawn::Engine::Window Arawn::Engine::createWindow() {
-	glfwInit();
-	GLFWwindow* window = glfwCreateWindow(800, 600, "", nullptr, nullptr);
-	if (!window) throw std::runtime_error("failed to create window");
-	return Window(window, glfwDestroyWindow);
+Arawn::Engine::VmaAllocatorRAII Arawn::Engine::createAllocator() {
+	VmaAllocatorCreateInfo info = {};
+    info.vulkanApiVersion = VK_API_VERSION_1_3;
+    info.physicalDevice = *gpu;
+    info.device = *device;
+    info.instance = *instance;
+	
+	
+    VmaAllocator allocator;
+    if (vmaCreateAllocator(&info, &allocator) != VK_SUCCESS) {
+		throw std::runtime_error("failed to init vma allocator");
+	}
+	return VmaAllocatorRAII(allocator);
 }
 
-vk::raii::SurfaceKHR createSurface(const vk::raii::Instance& instance, const Arawn::Engine::Window& window) {
-	VkSurfaceKHR surface;
-	glfwCreateWindowSurface(*instance, window.get(), nullptr, &surface);
-	return vk::raii::SurfaceKHR(instance, surface);
-}
 
 vk::raii::SwapchainKHR Arawn::Engine::createSwapchain() {
 	vk::SurfaceCapabilitiesKHR capabilities = gpu.getSurfaceCapabilitiesKHR(surface);
-	auto formats = gpu.getSurfaceFormatsKHR(surface);
+	
+	VkExtent2D extent;
+	if (capabilities.currentExtent.width != UINT32_MAX)
+	{
+		extent.width  = std::clamp(capabilities.currentExtent.width,  capabilities.minImageExtent.width,  capabilities.maxImageExtent.width);
+		extent.height = std::clamp(capabilities.currentExtent.height, capabilities.minImageExtent.height, capabilities.maxImageExtent.height);
+	}
+	
+	uint32_t imageCount;
+	imageCount = settings.display.buffering == BufferingMode::TRIPLE ? 3 : 2;
+	imageCount = std::max(imageCount, capabilities.minImageCount);
+	if (capabilities.maxImageCount > 0) {
+		imageCount = std::min(imageCount, capabilities.maxImageCount);
+	}
+
+	vk::PresentModeKHR presentMode;
 	auto presentModes = gpu.getSurfacePresentModesKHR(surface);
-
-	vk::SurfaceFormatKHR surfaceFormat = formats[0];
-	for (const auto& format : formats) {
-		if (format.format == vk::Format::eB8G8R8A8Srgb && format.colorSpace == vk::ColorSpaceKHR::eSrgbNonlinear) {
-
+	if (settings.display.vsync == VsyncMode::ENABLED) {
+		switch (settings.display.latency) {
+		case LowLatencyMode::ENABLED:
+		case LowLatencyMode::BALANCED:
+			if (std::ranges::contains(presentModes, vk::PresentModeKHR::eMailbox)) {
+				presentMode = vk::PresentModeKHR::eMailbox;
+				break;
+			}
+		case LowLatencyMode::DISABLED:
+			presentMode = vk::PresentModeKHR::eFifo;
+			break;
+		}
+	} else {
+		switch (settings.display.latency) {
+		case LowLatencyMode::ENABLED:
+			if (std::ranges::contains(presentModes, vk::PresentModeKHR::eImmediate)) {
+				presentMode = vk::PresentModeKHR::eImmediate;
+				break;
+			}
+		case LowLatencyMode::BALANCED:
+		case LowLatencyMode::DISABLED:
+			if (std::ranges::contains(presentModes, vk::PresentModeKHR::eFifoRelaxed)) {
+				presentMode = vk::PresentModeKHR::eFifoRelaxed;
+				break;
+			}
+			
+			presentMode = vk::PresentModeKHR::eFifo;
+			break;
 		}
 	}
 
+	auto surfaceFormats = gpu.getSurfaceFormatsKHR(surface);
+	surfaceFormat = surfaceFormats.front();
+	for (const auto& candidate : surfaceFormats) { // if preferred format
+		switch (candidate.format)
+		{
+			case(vk::Format::eR8G8B8A8Srgb): break;
+			case(vk::Format::eB8G8R8A8Srgb): break;
+			case(vk::Format::eR8G8B8A8Unorm): break;
+			case(vk::Format::eB8G8R8A8Unorm): break;
+			default: continue;
+		}
+		surfaceFormat = candidate;
+	}	
 
+	vk::SwapchainCreateInfoKHR info{ 
+		{}, 
+		surface, 
+		imageCount, 
+		surfaceFormat.format, 
+		surfaceFormat.colorSpace, 
+		extent,
+		1, 
+		vk::ImageUsageFlagBits::eColorAttachment,
+		vk::SharingMode::eExclusive, 
+		{},
+		capabilities.currentTransform,
+		vk::CompositeAlphaFlagBitsKHR::eOpaque,
+		presentMode,
+		VK_TRUE,
+		nullptr
+	};
+
+	return vk::raii::SwapchainKHR(device, info);
+
+}
+
+void Arawn::Engine::VmaAllocatorDeleter::operator()(VmaAllocator_T* ptr) {
+	vmaDestroyAllocator(ptr);
+}
+void Arawn::Engine::GLFWwindowDeleter::operator()(GLFWwindow* ptr) {
+	glfwDestroyWindow(ptr);
 }
